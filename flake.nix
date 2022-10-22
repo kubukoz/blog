cafa5c2f891ede4a964269adf859e79e8937f6d4
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-22 20:17:34 +0200

    remove legacy

--- flake.nix
+++ flake.nix
@@ -1,28 +1,22 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      gems = pkgs.bundlerEnv {
-        name = "blog-gems";
-        inherit (pkgs) ruby;
-        gemdir = ./.;
-      };
     in
     {
       devShells.default =
         pkgs.mkShell {
           buildInputs = [ pkgs.zola ];
         };
-      packages.default = pkgs.stdenv.mkDerivation
-        {
-          pname = "kubukoz-blog";
-          version = "1.0.0";
-          buildInputs = [ pkgs.zola ];
-          src = ./.;
-          buildPhase = "zola build";
-          installPhase = "cp -r public $out";
-        };
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "kubukoz-blog";
+        version = "1.0.0";
+        buildInputs = [ pkgs.zola ];
+        src = self;
+        buildPhase = "zola build";
+        installPhase = "cp -r public $out";
+      };
     });
 }
