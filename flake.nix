fac99e32b2f2c3d57d0aa84e60e38838b7809f4b
Jakub Kozłowski <kubukoz@gmail.com>
2023-02-15 03:45:38 +0100

    Try to deploy preview

--- flake.nix
+++ flake.nix
@@ -5,24 +5,28 @@
     let
       pkgs = import nixpkgs { inherit system; };
       mdoc = pkgs.callPackage ./mdoc.nix { inherit (pkgs.callPackage ./coursier.nix { }) coursier-tools; };
-    in
-    {
-      # Intermediate package with just mdoc, for "watch" mode
-      packages.mdoc_outputs = mdoc.mdoc_outputs;
-
-      packages.default = pkgs.stdenv.mkDerivation {
+      page = { base-url ? null }: pkgs.stdenv.mkDerivation {
         name = "kubukoz-blog";
         buildInputs = [ pkgs.zola ];
         src = self;
 
         inherit (mdoc) mdoc_outputs;
-        buildPhase = ''
-          cp $mdoc_outputs/* content
-          zola build --output-dir $out
-        '';
+        buildPhase =
+          let baseUrlString = if base-url == null then "" else "--base-url ${base-url}"; in
+          ''
+            cp $mdoc_outputs/* content
+            zola build --output-dir $out ${baseUrlString}
+          '';
 
         dontInstall = true;
       };
+    in
+    {
+      # Intermediate package with just mdoc, for "watch" mode
+      packages.mdoc_outputs = mdoc.mdoc_outputs;
+
+      packages.default = page { };
+      packages.preview = page { base-url = "https://blog.kubukoz.com/preview"; };
 
       checks.default = self.packages.${system}.default;
     });
