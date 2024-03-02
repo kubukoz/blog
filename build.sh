b2a8f3fd2af4c689da3a7e40216508b3524f2615
Jakub Kozłowski <kubukoz@gmail.com>
2024-03-02 17:07:44 +0100

    document workaround

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 PACKAGE_PATH="$1"
 
+# Shallow + ensuring there's a hash: workaround for https://github.com/NixOS/nix/pull/10125
 nix build "git+file://$(pwd)/.?shallow=1&submodules=1#${PACKAGE_PATH:-default}" --print-build-logs
