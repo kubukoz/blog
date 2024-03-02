c16be06cb290bcf347ddf3e689e87c95dfff0021
Jakub Kozłowski <kubukoz@gmail.com>
2024-03-02 17:08:49 +0100

    re-add shallow

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 PACKAGE_PATH="$1"
 
-# there must be a hash: workaround for https://github.com/NixOS/nix/pull/10125
-nix build "git+file://$(pwd)/.?submodules=1#${PACKAGE_PATH:-default}" --print-build-logs
+# shallow + there must be a hash: workaround for https://github.com/NixOS/nix/pull/10125
+nix build "git+file://$(pwd)/.?shallow=1&submodules=1#${PACKAGE_PATH:-default}" --print-build-logs
