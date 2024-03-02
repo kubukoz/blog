9ab0c963a82b2bc950ce87af72542971c36c43a9
Jakub Kozłowski <kubukoz@gmail.com>
2024-03-02 17:05:34 +0100

    ultimate fix attempt

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 PACKAGE_PATH="$1"
 
-nix build "git+file://$(pwd)/.?shallow=1&submodules=1${PACKAGE_PATH:-}" --print-build-logs
+nix build "git+file://$(pwd)/.?shallow=1&submodules=1#${PACKAGE_PATH:-default}" --print-build-logs
